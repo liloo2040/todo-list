1a68a319d66b1dd8f9ccad403050112a8cffbfdb
liloo2040 <e.dufour.89@gmail.com>
2018-02-03 13:05:46 +0100

    adding todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'todo-root',
   template: `<h1>{{title}}</h1>
-  
-  <todo-input></todo-input>`,
+  <todo-input></todo-input>
+  <ul>
+  <li *ngFor="let item of todoList">
+  {{ item.title }}</li>
+  </ul>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo';
+  todoList =[
+    {title: 'install Node JS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }
