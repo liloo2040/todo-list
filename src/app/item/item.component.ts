03276bd5e172324615fd2be673c793953f2d4203
liloo2040 <e.dufour.89@gmail.com>
2018-02-04 17:05:39 +0100

    adding completed item

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -4,7 +4,8 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'todo-item',
   template: `
-    <p>
+    <input type="checkbox" (click)="completeItem()"/>
+    <p class="todo-title" [ngClass]="{'todo-complete': isComplete}">
       {{ todoItem.title }}
     </p>
     <button class="btn btn-red" (click)="removeItem()">remove</button>
@@ -16,9 +17,15 @@ export class ItemComponent implements OnInit {
   @Output() remove:EventEmitter<any> = new EventEmitter();
 
   @Input() todoItem: any;
+
   removeItem() {
     this.remove.emit(this.todoItem);
   }
+  isComplete: boolean = false;
+  completeItem() {
+    this.isComplete = !this.isComplete;
+  }
+
   constructor() { }
 
   ngOnInit() {
